267eb388d659263fb63b1a691ccdb1068604b8b2
Rana Hammad <hammadrana@truedata.co>
2024-11-23 21:37:31 +0500

    roles and permission management

--- api/js/api/start/routes.ts
+++ api/js/api/start/routes.ts
@@ -1,12 +1,3 @@
-/*
-|--------------------------------------------------------------------------
-| Routes file
-|--------------------------------------------------------------------------
-|
-| The routes file is used for defining the HTTP routes.
-|
-*/
-
 import router from '@adonisjs/core/services/router'
 import {middleware} from "#start/kernel";
 const UsersController = () => import('#controllers/users_controller');
