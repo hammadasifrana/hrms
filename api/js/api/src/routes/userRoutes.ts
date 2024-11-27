ea89e0c6e8a23da4ba28de8d7f762853d5ce8a0b
Rana Hammad <hammadrana@truedata.co>
2024-11-27 16:29:53 +0500

    enabling user route

--- api/js/api/src/routes/userRoutes.ts
+++ api/js/api/src/routes/userRoutes.ts
@@ -4,22 +4,26 @@ import { requireAuth, checkPermission } from '../middlewares/authMiddleware';
 
 const router = express.Router();
 
-router.get('/', requireAuth,
+router.get('/',
+  requireAuth,
   checkPermission('users:read'),
   listUsers
 );
 
 router.get('/:id',
+  requireAuth,
   checkPermission('users:read'),
   getUser
 );
 
 router.put('/:id',
+  requireAuth,
   checkPermission('users:update'),
   updateUser
 );
 
 router.delete('/:id',
+  requireAuth,
   checkPermission('users:delete'),
   deleteUser
 );
