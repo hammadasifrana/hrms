7d8bd19a2422c49b8b7422ddcc97f3a29d7b28a9
Sajid Ali Mudassar <sajidalimudassar@gmail.com>
2024-11-27 15:37:55 +0500

    esm module and docker

--- api/js/api/src/config/environment.ts
+++ api/js/api/src/config/environment.ts
@@ -1,6 +1,9 @@
 import dotenv from 'dotenv';
 import path from 'path';
+import { fileURLToPath } from 'url';
 
+const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
+const __dirname = path.dirname(__filename); // get the name of the directory
 dotenv.config({ path: path.resolve(__dirname, '../../.env') });
 
 export const environment = {
